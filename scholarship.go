40e88d40421803aad1d0f4ce52cbaeca957b12d8
Habib Fikri <habibgarut@gmail.com>
2018-04-23 03:03:52 +0700

    Change startdate and enddate type to time

--- scholarship.go
+++ scholarship.go
@@ -10,11 +10,15 @@ type Scholarship struct {
 	Country        string    `db:"country" json:"country"`
 	Flag           string    `db:"flag" json:"flag"`
 	State          int       `db:"state" json:"state"`
-	StartDate      string    `db:"start_date" json:"start_date"`
-	EndDate        string    `db:"end_date" json:"end_date"`
+	StartDate      time.Time `db:"start_date" json:"start_date"`
+	EndDate        time.Time `db:"end_date" json:"end_date"`
 	Description    string    `db:"description" json:"description"`
 	Requirement    string    `db:"requirement" json:"requirement"`
 	OrganizationID int       `db:"organization_id" json:"organization_id"`
 	CreatedAt      time.Time `db:"created_at" json:"created_at"`
 	UpdatedAt      time.Time `db:"updated_at" json:"updated_at"`
+}
+
+func (s *Scholarship) Validate() error {
+	return nil
 }
