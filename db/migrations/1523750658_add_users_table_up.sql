e471cba355596637140b10ac6a40a9ef3e2cb1eb
Habib Fikri <habibgarut@gmail.com>
2018-05-18 00:24:05 +0700

    Add default value

--- db/migrations/1523750658_add_users_table_up.sql
+++ db/migrations/1523750658_add_users_table_up.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
 	id int(11) NOT NULL AUTO_INCREMENT,
 	email_address varchar(50) NOT NULL UNIQUE,
 	password varchar(255) NOT NULL,
-	role varchar(50) NOT NULL,
+	role varchar(50) NOT NULL DEFAULT "",
 	created_at datetime NOT NULL,
 	updated_at datetime NOT NULL,
 	PRIMARY KEY (id),
--- db/migrations/1524274713_add_student_boards_table_up.sql
+++ db/migrations/1524274713_add_student_boards_table_up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE student_boards (
     id int(11) NOT NULL AUTO_INCREMENT,
-    name varchar(100) NOT NULL,
+    name varchar(100) NOT NULL DEFAULT '',
     category int(2) NOT NULL,
-    description longtext NOT NULL,
+    description longtext NOT NULL DEFAULT '',
     state int(2) NOT NULL,
     user_id int(11) NOT NULL,
     created_at datetime NOT NULL,
